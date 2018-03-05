dfbd043f1586d4fe0995a98e233c76477ec51e3b
WangRuning <wa123fei@gmail.com>
2018-03-06 00:10:34 +0800

    1.比较二叉树 2.判断括号是否有效

---
+++ test/com/test/leetcode/ValidParenthesesTest.java
@@ -0,0 +1,14 @@
+package com.test.leetcode;
+
+import edu.princeton.cs.algs4.StdOut;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidParenthesesTest {
+
+  @Test
+  public void isValid() {
+    StdOut.println(new ValidParentheses().isValid("{()}"));
+  }
+}
